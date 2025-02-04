caceb5fbde813ca4452f03457c133927ecd07314
shifna majeed <sshifnamajeed@gmail.com>
2025-02-04 22:17:20 +0530

    updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5175",
+    origin:"http://localhost:5173",
     credentials:true,
 }))
 
