b436d2b8e8e1955db0f02022a188fd5439304d1d
shifna majeed <sshifnamajeed@gmail.com>
2025-02-04 20:56:47 +0530

    task completed

---
+++ Backend/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import {connectDB} from "./lib/db.js"
+import cookieParser from "cookie-parser"
+import messageRoutes from './routes/message.route.js'
+import cors from "cors"
+import {app,server} from "./lib/socket.js"
+
+dotenv.config()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5175",
+    credentials:true,
+}))
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
+
+server.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:5001`);
+    connectDB()
+})
---
+++ Frontend/src/lib/axios.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  withCredentials: true,
+});
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
